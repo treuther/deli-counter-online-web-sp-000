ec169b852d7c5532d29e3921d00e87e9776ae39a
Tracy Reuther <tracyreuther@gmail.com>
2019-05-28 09:21:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,5 +21,9 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}"
+    katz_deli.shift
 end
