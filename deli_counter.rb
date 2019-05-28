31293f8f8fbae208b05c940ab4a31b9a121989e3
Tracy Reuther <tracyreuther@gmail.com>
2019-05-28 09:10:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,5 +2,14 @@
 katz_deli = []
 
 def line(katz_deli)
-
+  position = []
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    number = 1
+    katz_deli.each do |customer|
+      position << "#{number}. #{customer}"
+      number += 1
+    end
+    puts "The line is currently: #{position}"
 end
