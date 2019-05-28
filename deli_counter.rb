1385b02a1c31031896ac72d2e8e99d42a073f953
Tracy Reuther <tracyreuther@gmail.com>
2019-05-28 09:04:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,14 +2,5 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.length > 0
-    puts "The line is currently: #{katz_deli.index - 1}. "
-  else
-    puts "The line is currently empty."
-  end
-end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
