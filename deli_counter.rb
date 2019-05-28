50df159eb55f4659e0fc38b7657806af5b210f23
Tracy Reuther <tracyreuther@gmail.com>
2019-05-28 08:44:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,9 @@
 # Write your code here.
 def line(katz_deli)
-  if katz_deli > 0
+  if katz_deli.length > 0
+    puts "The line is currently: #{katz_deli.length}"
+  else
+    puts "The line is currently empty."
 end
 
 def take_a_number(katz_deli, name)
