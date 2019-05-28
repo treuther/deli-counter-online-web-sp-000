621bf93081514f1d283188c64136d223e99eb003
Tracy Reuther <tracyreuther@gmail.com>
2019-05-28 09:01:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,9 @@
 # Write your code here.
+katz_deli = []
+
 def line(katz_deli)
   if katz_deli.length > 0
-    puts "The line is currently: #{katz_deli.length}"
+    puts "The line is currently: #{katz_deli.index - 1}. "
   else
     puts "The line is currently empty."
   end
